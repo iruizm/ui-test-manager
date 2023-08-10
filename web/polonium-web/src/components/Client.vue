e0500790fe09557b29226ecc01790d6de831e647
iruiz <14930457-iruizm@users.noreply.gitlab.com>
2023-08-10 15:09:17 +0200

    add api client and event bus

---
+++ web/polonium-web/src/components/Client.vue
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      responseData: null,
+    };
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const response = await axios.get('http://your-go-api-url/api/endpoint');
+        this.responseData = response.data;
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    },
+  },
+  mounted() {
+    this.fetchData();
+  },
+};
--- web/polonium-web/src/main.js
+++ web/polonium-web/src/main.js
@@ -3,4 +3,8 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import mitt from 'mitt';
+const emitter = mitt();
+app.config.globalProperties.emitter = emitter;
+
 createApp(App).mount('#app')
